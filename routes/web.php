c184b7a12baed4352712ecc13f97d982eafb1b23
Luis Mayta <luis.mayta@gmail.com>
2017-08-22 20:29:26 -0500

    Limpiar

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__.'/home.route.php';
-require __DIR__.'/catalogo.route.php';
-require __DIR__.'/users.route.php';
-
-Route::get('logitos', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-
-Auth::routes();
