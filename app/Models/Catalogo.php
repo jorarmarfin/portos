bee6d0c4e815c60b54654f2faef6862188a97814
Luis Mayta <luis.mayta@gmail.com>
2018-02-16 10:44:33 -0500

    Nueva Instalacion

---
+++ app/Models/Catalogo.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catalogo extends Model
+{
+    protected $table = 'catalogo';
+    protected $fillable = ['idtable', 'iditem', 'codigo','nombre','descripcion','valor','activo'];
+    public $timestamps = false;
+    /**
+     * Busca una tabla de maestro
+     */
+	public function Maestro($NameTable){
+		$NameTable = strtoupper($NameTable);
+		$data=$this->select('iditem')
+				   ->where('idtable',0)
+			       ->where('nombre',"$NameTable")->first();
+		return $data->iditem;
+	}
+	/**
+	 *Muestra una lista desplegable para un input select
+	 */
+	public function scopeCombo($cadenaSQL,$NameTable){
+		$idtable=$this->Maestro($NameTable);
+		return $cadenaSQL->where('idtable',$idtable)
+						 ->where('activo',1)
+						 ->orderBy('nombre');
+	}
+	/**
+	 * Devuelve el Id de un registro de una de las tablas de catalogo buscando por un campo
+	 *
+	 * @param      String  $NameTable     El nombre de la Tabla
+	 * @param      String  $field  		  El campo que se usara para buscar
+	 * @param      String  $Name		  El nombre del que se desea su id
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public function scopeIdCatalogo($cadenaSQL,$NameTable,$Field,$Name){
+		$idtable=$this->Maestro($NameTable);
+		$campo = ($Field=='C') ? 'codigo' : 'nombre' ;
+		return $cadenaSQL->where('idtable',$idtable)
+						 ->where($campo,$Name)
+						 ->where('activo',1)->pluck('id')[0];
+	}
+	 #--------------------------------------------------------------------
+    public function scopeTable($cadenaSQL,$NameTable){
+        $NameTable = strtoupper($NameTable);
+        $idtable=$this->Maestro($NameTable);
+        return $cadenaSQL->where('idtable',$idtable)
+                         ->orderBy('iditem','asc');
+    }
+    #--------------------------------------------------------------------
+    public function scopeIdtable($cadenaSQL,$NameTable){
+        $NameTable = strtoupper($NameTable);
+        return $cadenaSQL->select('iditem')
+                         ->where('idtable',0)
+                         ->where('nombre',"$NameTable")
+                         ->where('activo',1)
+                         ->first();
+    }
+    /**
+     * Devuelve la tabla buscando por el id de la tabla
+     * @param integer $iditem idtable
+     */
+    public static function SearchTable($iditem)
+    {
+    	$catalogo = Catalogo::select('nombre')->where('idtable',0)->where('iditem',$iditem)->first();
+    	return Catalogo::Table($catalogo->nombre);
+    }
+    /**
+     * Devuelve el nombre de la tabla buscando por el id de la tabla
+     * @param integer $iditem idtable
+     */
+    public static function SearchNameTable($iditem)
+    {
+    	$catalogo = Catalogo::select('nombre')->where('idtable',0)->where('iditem',$iditem)->first();
+    	return $catalogo->nombre;
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('idrole')->unsigned();
+            $table->string('foto',50)->default('nofoto.jpg');
+            $table->boolean('activo')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('idrole')->references('id')->on('catalogo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
