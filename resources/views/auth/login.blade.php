8ad95dddb9d1ebc8d865a746597e53d4e5a8da53
Luis Mayta <luis.mayta@gmail.com>
2018-02-16 18:26:19 -0500

    Base admin y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.login4')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Login</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+@section('login')
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Address</label>
+{!! Form::open(['route'=>'login','method'=>'POST','class'=>'m-login__form m-form']) !!}
+        <div class="form-group m-form__group">
+            {!! Form::text('username', '', ['placeholder'=>'username','class'=>'form-control m-input','autocomplete'=>'off']) !!}
+        </div>
+        <div class="form-group m-form__group">
+            {!! Form::password('password', ['placeholder'=>'Password','class'=>'form-control m-input m-login__form-input--last']) !!}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        </div>
+        <div class="row m-login__form-sub">
+            <div class="col m--align-left m-login__form-left">
+                <label class="m-checkbox  m-checkbox--light">
+                    <input type="checkbox" name="remember">
+                    Recuerdame
+                    <span></span>
+                </label>
+            </div>
+            <div class="col m--align-right m-login__form-right">
+                <a href="javascript:;" id="m_login_forget_password" class="m-link">
+                    ¿ Olvidaste tu clave ?
+                </a>
             </div>
         </div>
-    </div>
-</div>
+        <div class="m-login__form-action">
+            {!! Form::submit('Ingresar',['class'=>'btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn']) !!}
+        </div>
+{!! Form::close() !!}
 @endsection
