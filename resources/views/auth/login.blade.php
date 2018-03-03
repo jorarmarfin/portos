08abac6bc2d9389016ed21539a0a6b78952c6be5
Luis Mayta <luis.mayta@gmail.com>
2018-03-03 00:28:36 -0500

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,43 @@
-@extends('layouts.login4')
+@extends('layouts.login')
 
 
 @section('login')
 @include('layouts.alerts.errors')
 {!! Form::open(['route'=>'login','method'=>'POST','class'=>'m-login__form m-form']) !!}
-        <div class="form-group m-form__group">
-            {!! Form::text('username', '', ['placeholder'=>'username','class'=>'form-control m-input','autocomplete'=>'off']) !!}
-        </div>
-        <div class="form-group m-form__group">
-            {!! Form::password('password', ['placeholder'=>'Password','class'=>'form-control m-input m-login__form-input--last']) !!}
-
-        </div>
-        <div class="row m-login__form-sub">
-            <div class="col m--align-left m-login__form-left">
-                <label class="m-checkbox  m-checkbox--light">
-                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                    Recuerdame
-                    <span></span>
-                </label>
-            </div>
-            <div class="col m--align-right m-login__form-right">
-                <a href="javascript:;" id="m_login_forget_password" class="m-link">
-                    ¿ Olvidaste tu clave ?
-                </a>
+        <div class="form-group mb-3">
+            <label>Username</label>
+            <div class="input-group input-group-icon">
+                {!! Form::text('username', '', ['placeholder'=>'username','class'=>'form-control form-control-lg']) !!}
+                <span class="input-group-addon">
+                    <span class="icon icon-lg">
+                        <i class="fa fa-user"></i>
+                    </span>
+                </span>
             </div>
         </div>
-        <div class="m-login__form-action">
-            {!! Form::submit('Ingresar',['class'=>'btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn']) !!}
-        </div>
+        <div class="form-group mb-3">
+            <div class="clearfix">
+                <label class="float-left">Password</label>
+                <a href="pages-recover-password.html" class="float-right">Olvidaste tu clave?</a>
+            </div>
+            <div class="input-group input-group-icon">
+                {!! Form::password('password', ['placeholder'=>'Password','class'=>'form-control form-control-lg']) !!}
+                <span class="input-group-addon">
+                    <span class="icon icon-lg">
+                        <i class="fa fa-lock"></i>
+                    </span>
+                </span>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="checkbox-custom checkbox-default">
+                        <input id="RememberMe" name="rememberme" type="checkbox"/>
+                        <label for="RememberMe">Recuerdame</label>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-right">
+                    {!! Form::submit('Ingresar',['class'=>'btn btn-primary mt-2']) !!}
+                </div>
+            </div>
 {!! Form::close() !!}
 @endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -41,7 +41,7 @@
 		<section class="body-sign">
 			<div class="center-sign">
 				<a href="/" class="logo float-left">
-					<img src="img/logo.png" height="54" alt="Porto Admin" />
+					<img src="{{ asset('img/logo.png') }}" height="54" alt="Porto Admin" />
 				</a>
 
 				<div class="panel card-sign">
@@ -49,58 +49,7 @@
 						<h2 class="title text-uppercase font-weight-bold m-0"><i class="fa fa-user mr-1"></i> Sign In</h2>
 					</div>
 					<div class="card-body">
-						<form action="index.html" method="post">
-							<div class="form-group mb-3">
-								<label>Username</label>
-								<div class="input-group input-group-icon">
-									<input name="username" type="text" class="form-control form-control-lg" />
-									<span class="input-group-addon">
-										<span class="icon icon-lg">
-											<i class="fa fa-user"></i>
-										</span>
-									</span>
-								</div>
-							</div>
-
-							<div class="form-group mb-3">
-								<div class="clearfix">
-									<label class="float-left">Password</label>
-									<a href="pages-recover-password.html" class="float-right">Lost Password?</a>
-								</div>
-								<div class="input-group input-group-icon">
-									<input name="pwd" type="password" class="form-control form-control-lg" />
-									<span class="input-group-addon">
-										<span class="icon icon-lg">
-											<i class="fa fa-lock"></i>
-										</span>
-									</span>
-								</div>
-							</div>
-
-							<div class="row">
-								<div class="col-sm-8">
-									<div class="checkbox-custom checkbox-default">
-										<input id="RememberMe" name="rememberme" type="checkbox"/>
-										<label for="RememberMe">Remember Me</label>
-									</div>
-								</div>
-								<div class="col-sm-4 text-right">
-									<button type="submit" class="btn btn-primary mt-2">Sign In</button>
-								</div>
-							</div>
-
-							<span class="mt-3 mb-3 line-thru text-center text-uppercase">
-								<span>or</span>
-							</span>
-
-							<div class="mb-1 text-center">
-								<a class="btn btn-facebook mb-3 ml-1 mr-1" href="#">Connect with <i class="fa fa-facebook"></i></a>
-								<a class="btn btn-twitter mb-3 ml-1 mr-1" href="#">Connect with <i class="fa fa-twitter"></i></a>
-							</div>
-
-							<p class="text-center">Don't have an account yet? <a href="pages-signup.html">Sign Up!</a></p>
-
-						</form>
+					@yield('login')
 					</div>
 				</div>
 
