f7448a67372cb3560883430942ab1e0ff29ce6bb
Luis Mayta <luis.mayta@gmail.com>
2018-03-04 17:42:24 -0500

    portos listo

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Homecontroller@index')->name('home.index');
 
-Route::get('logitos', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 Auth::routes();
-Route::get('/home', 'Admin\AdminController@index')->name('home');
-
-
+Route::get('logitos', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 Route::namespace('Admin')->middleware('auth')->name('admin.')->group(function(){
 	require __DIR__.'/admin/AdminRoute.php';
-
 });
 Route::namespace('Admin\Catalogo')->middleware('auth')->name('admin.')->group(function(){
 	require __DIR__.'/catalogo/CatalogoRoute.php';
